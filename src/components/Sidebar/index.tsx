8b3c8ea3b0c942a233806710a1210e9a763bebc9
EllenBessa <ellenbessa96@gmail.com>
2023-08-25 12:42:04 -0300

    feat: :sparkles: Pattern de Composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,41 +1,54 @@
-import { NavItem } from "./NavItem"
+import { NavItem } from "./NavItem";
 import { Profile } from "./Profile";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
+import * as Input from "../Input";
 
 import { Logo } from "./Logo";
-import {Search ,Home, BarChart, SquareStack, CheckSquare, Flag, Users, LifeBuoy, Cog} from "lucide-react"
+import {
+  Search,
+  Home,
+  BarChart,
+  SquareStack,
+  CheckSquare,
+  Flag,
+  Users,
+  LifeBuoy,
+  Cog,
+} from "lucide-react";
 
 export function Sidebar() {
-  return(
-    <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8'>
-      <Logo/>
+  return (
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500"/>
-        <input type="text" placeholder="Search" className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"/>
-      </div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <nav className="space-y-0.5">
-        <NavItem title="Home" icon={Home}/>
-        <NavItem title="Dashboard" icon={BarChart}/>
-        <NavItem title="Projects" icon={SquareStack}/>
-        <NavItem title="Tasks" icon={CheckSquare}/>
-        <NavItem title="Reporting" icon={Flag}/>
-        <NavItem title="Users" icon={Users}/>
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
       </nav>
 
-        <div className="mt-auto flex flex-col gap-6">
-          <nav className="space-y-0.5">
-            <NavItem title="Support" icon={LifeBuoy}/>
-            <NavItem title="Settings" icon={Cog}/>
-          </nav>
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
 
-          <UsedSpaceWidget/>
+        <UsedSpaceWidget />
 
-          <div className="h-px bg-zinc-200"></div>
+        <div className="h-px bg-zinc-200"></div>
 
-          <Profile/>
-        </div>
+        <Profile />
+      </div>
     </aside>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,12 @@ const config: Config = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-     gridTemplateColumns: {
-      app: "minmax(18rem, 20rem) 1fr",
-      profile: "max-content 1fr min-content"
-     }
-    }
+      gridTemplateColumns: {
+        app: "minmax(18rem, 20rem) 1fr",
+        profile: "max-content 1fr min-content",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
 export default config;
