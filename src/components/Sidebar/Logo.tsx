fbffc3205c7bf4b562d11cb51acdb7c836a6db67
EllenBessa <ellenbessa96@gmail.com>
2023-08-25 11:08:26 -0300

    feat: :lipstick: Menu de Navegação

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+    <strong className="flex items-center gap-2 text-xl mx-1 font-semibold text-zinc-900">
     <svg
       width={38}
       height={38}
