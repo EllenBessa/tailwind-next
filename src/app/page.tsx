e01defddd46ad513bd4601837ece88d7bed682f3
EllenBessa <ellenbessa96@gmail.com>
2023-09-11 01:19:14 -0300

    feat: :sparkles: Variantes de Botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { SelectItem } from "@/components/Form/Select/SelectItem";
 
 import { Bold, Italic, Link, List, ListOrdered, Mail } from "lucide-react";
 import { Textarea } from "@/components/Form/Textarea";
+import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
@@ -23,19 +24,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button type="submit" form="settings" variant="primary">
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -225,19 +219,12 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button type="submit" form="settings" variant="primary">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
