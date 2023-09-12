9ff21c863a7d273f2f0c88e03acb938ceea76043
EllenBessa <ellenbessa96@gmail.com>
2023-09-12 14:02:26 -0300

    feat: :lipstick: Tema dark da sidebar

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -11,8 +11,10 @@ export function Profile() {
       />
 
       <div className="flex flex-1 flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">Ellen Bessa</span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Ellen Bessa
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           ellen@rocketseat.com.br
         </span>
       </div>
