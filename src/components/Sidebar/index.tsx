278d2c7ff29abe45deb2cf0fd480bf07d74700ca
EllenBessa <ellenbessa96@gmail.com>
2023-09-12 10:46:42 -0300

    refactor: :recycle: Arrumadno a sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -23,7 +23,7 @@ import { Button } from "../Button";
 
 export function Sidebar() {
   return (
-    <Collapsible.Root className="fixed bottom-0 left-0 right-0 top-0 z-20 flex h-screen flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:relative lg:right-auto lg:w-80 lg:border-r  lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 overflow-hidden border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:bottom-0 lg:right-auto lg:h-auto lg:w-80 lg:overflow-auto lg:border-b-0 lg:border-r lg:px-5 lg:py-8">
       <div className="flex items-center justify-between">
         <Logo />
         <Collapsible.Trigger className="lg:hidden">
